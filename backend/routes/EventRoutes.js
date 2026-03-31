96d1aad9872b1f2eb2a76574997c409930d266b2
mgoel-01 <goelmanthan03@gmail.com>
2026-03-31 23:51:44 +0530

    added update event route

--- backend/routes/EventRoutes.js
+++ backend/routes/EventRoutes.js
@@ -66,6 +66,29 @@ router.delete("/:id",async(req,res)=>{
     }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedEvent) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    res.json({
+      message: "Event updated successfully",
+      event: updatedEvent
+    });
+
+  } catch (error) {
+    console.log("UPDATE ERROR:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 router.get("/:id", async (req, res) => {
   try {
     const event = await Event.findById(req.params.id);
