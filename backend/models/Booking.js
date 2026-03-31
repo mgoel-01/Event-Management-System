cff9741268cf03b2cab4c2d69fa79812f229e955
mgoel-01 <goelmanthan03@gmail.com>
2026-03-31 23:18:38 +0530

    fixed eventId type in booking schema

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
   userId: String,
-  eventId: Number,
+  eventId: String,
   eventName: String,
   quantity: Number,
   totalAmount: Number,
